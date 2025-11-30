0fe8ac124454926d0a22a0179201597d68eeba45
julesss-coder <julesss@posteo.de>
2025-11-30 12:18:00 +0100

    Generate uncompressed and compressed pubkeys, pubkey hash.

--- learning_bitcoin.py
+++ learning_bitcoin.py
@@ -2,12 +2,15 @@ from ecpy.curves import Curve
 from ecpy.keys import ECPrivateKey
 from cmath import sqrt
 import hashlib
+print(hashlib.algorithms_available)
 
 class KeyPair:
     def __init__(self):
         self.private_key:int = self.generate_private_key() #TODO Make inaccessible
-        self.public_key:dict[str, int] = self.generate_public_key()
-        self.compressed_public_key:str = self.compress_public_key()
+        self.public_key_coordinates:dict[str, int] = self.generate_public_key_coordinates()
+        self.uncompressed_public_key:bytes = self.generate_uncompressed_public_key()
+        self.compressed_public_key:str = self.generate_compressed_public_key()
+        self.public_key_hash:str = self.generate_public_key_hash()
 
     def generate_private_key(self)->int:
         # get reliable source of randomness from Linux
@@ -25,32 +28,51 @@ class KeyPair:
         return decimal_format
 
 
-    def generate_public_key(self)->dict[str: int]:
+    def generate_public_key_coordinates(self)->dict[str:int]:
+        """
+        Generates the x and y coordinates of the public key.
+        """
         # Generator point
         curve = Curve.get_curve('secp256k1')
         G = curve.generator
 
         # Generate public key
         public_key = self.private_key * G
-        x = hex(public_key.x)[2:]
-        y = hex(public_key.y)[2:]
-        return {'x': x, 'y': y}
-    
+        return {
+            'x': public_key.x,
+            'y': public_key.y
+        }
         #TODO Try with ECPrivateKey as well and compare results
     
 
-    def compress_public_key(self)->str:
-        return '02' + self.public_key['x'] if int(self.public_key['y'], 16) % 2 == 0 else '03' + self.public_key['x']
+    def generate_uncompressed_public_key(self)->str:
+        """
+        Generates an uncompressed public key.
+        """
+        x_bytes = self.public_key_coordinates['x'].to_bytes(32, "big")
+        y_bytes = self.public_key_coordinates['y'].to_bytes(32, "big")
+        uncompressed_public_key = b"\x04" + x_bytes + y_bytes
+        return uncompressed_public_key
+    
+
+    def generate_compressed_public_key(self)->str:
+        """
+        Generates compressed public key.
+        """
+        return '02' + hex(self.public_key_coordinates['x']) if self.public_key_coordinates['y'] % 2 == 0 else '03' + hex(self.public_key_coordinates['x'])
     
 
     def decompress_public_key(self)->dict[str, int]:
+        """
+        Returns the x and y coordinates of the public key.
+        """
         prefix = self.compressed_public_key[0:2]
         x = self.compressed_public_key[2:] 
         curve = Curve.get_curve('secp256k1')
         p = curve.field
 
         # solve curve equation: y^2 = x^3 + 7 mod p
-        y_squared = (pow(self.public_key['x'], 3, p) + 7) % p
+        y_squared = (pow(self.public_key_coordinates['x'], 3, p) + 7) % p
         # Modular square root
         y = pow(y_squared, (p + 1) // 4, p)
 
@@ -62,22 +84,21 @@ class KeyPair:
 
         y = hex(y)[2:]
 
-        return {'x': x, 'y': y}
+        return {'x': int(x, 16), 'y': int(y, 16)}
     
 
-    def generate_public_key_hash(self):
+    def generate_public_key_hash(self)->str:
         # sha-256
         # input for sha-256 is public key (compressed?)
-        # output is called digest
-        hash = hashlib.sha256()
-        hash.update(self.compressed_public_key.encode('utf-8'))
-        digest = hash.digest()
-        hexdigest = hash.hexdigest()
-        print("sha-256 digest: ", digest)
-        #TODO
+        sha256_hash = hashlib.sha256()
+        sha256_hash.update(self.compressed_public_key.encode('utf-8')) #encode argument to bytes-like object first
+        sha256_digest = sha256_hash.digest()
+        
         # ripemd60
-        # return address
-    
+        ripemd160_hash = hashlib.new('ripemd160')
+        ripemd160_hash.update(sha256_digest)
+        return ripemd160_hash.hexdigest()
+
 
     def generate_address(self):
         pass
@@ -86,7 +107,8 @@ class KeyPair:
 
 key_pair = KeyPair()
 print(key_pair)
-print(key_pair.generate_public_key_hash())
+print("key_pair.public_key_coordinates: ", key_pair.public_key_coordinates)
+print("decompressed public key", key_pair.decompress_public_key())
 
 
 class Transaction:
